b5350f56e454f8d92daeda3e1cccf45e9f414a66
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-12-03 19:08:58 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/Raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raytracer.dir/main.cpp.o"
+  "Raytracer.pdb"
+  "Raytracer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/red_triangle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/red_triangle.dir/main.cpp.o"
+  "red_triangle.pdb"
+  "red_triangle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/red_triangle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
